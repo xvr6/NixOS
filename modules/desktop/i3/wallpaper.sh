f3267cc9e678e4564170adf893bba21397283e4a
xvr6 <xvr0612@gmail.com>
2026-01-14 09:34:13 -0500

    removed unused desktop configs

--- modules/desktop/i3/wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for output in $(xrandr --listmonitors | awk 'NR>1 {print $4}'); do
-    feh --bg-fill "$1" --no-fehbg --output "$output"
-done
--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -15,14 +15,12 @@
       };
 
       scripts = [
-        (import ./rebuild.nix scriptArgs)
-        (import ./rollback.nix scriptArgs)
+        (import ./driverinfo.nix scriptArgs)
+        (import ./extract.nix scriptArgs)
         (import ./launcher.nix scriptArgs)
         (import ./network.nix scriptArgs)
+        (import ./rollback.nix scriptArgs)
         (import ./tmux-sessionizer.nix scriptArgs)
-        (import ./extract.nix scriptArgs)
-        (import ./driverinfo.nix scriptArgs)
-        (import ./underwatt.nix scriptArgs)
         # Add new scripts here as you create them
   ];
   in {
